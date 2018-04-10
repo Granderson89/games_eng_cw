b013b82bed874ad2766e33185959b71cd23a88ef
Paulius Bieksa <priverstinis@gmail.com>
2018-04-10 17:44:01 +0100

    Added a resource manager class.

---
+++ coursework/resource_manager.cpp
@@ -0,0 +1,35 @@
+#include "resource_manager.h"
+
+
+bool ResourceManager::Load()
+{
+	bool good = true;
+	bool test = true;
+	if (!tex_test.loadFromFile("res/img/test.png"))
+	{
+		cout << "Test image file not found!" << endl;
+		good = false;
+		test = false;
+	}
+	if (!Tex_background.loadFromFile("res/img/test.png"))
+	{
+		cout << "Background image file not found!" << endl;
+		good = false;
+		if (test)
+			Tex_background = tex_test;
+	}
+	if (!Tex_player1.loadFromFile("res/img/player1.png"))
+	{
+		cout << "Background image file not found!" << endl;
+		good = false;
+		if (test)
+			Tex_background = tex_test;
+	}
+	if (!Tex_player2.loadFromFile("res/img/player2.png"))
+	{
+		cout << "Background image file not found!" << endl;
+		good = false;
+		if (test)
+			Tex_background = tex_test;
+	}
+}
