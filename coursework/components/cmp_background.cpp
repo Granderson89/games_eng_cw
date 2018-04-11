3c828eedcc27d2592b1cb751d19bb179bc54c6cd
Paulius Bieksa <priverstinis@gmail.com>
2018-04-11 15:17:12 +0100

    The background is int the foregrond at the moment and it is a bot off-center.

--- coursework/components/cmp_background.cpp
+++ coursework/components/cmp_background.cpp
@@ -4,28 +4,32 @@
 
 int rows;
 int colls;
-Vector2f offset;
+Vector2f topLeft;
+Vector2u bgSize;
 
 BackgroundBuilderComponent::BackgroundBuilderComponent(Entity* p) : Component(p)
 {
 	_cam = p->GetCompatibleComponent<CameraControllerComponent>()[0];
+	bgSize = ResourceManager::Tex_background.getSize();
 
-	rows = _max_zoom / ResourceManager::Tex_background.getSize().x + 1;
-	colls = (_max_zoom * (_cam->getView().getSize().x / _cam->getView().getSize().y)) / ResourceManager::Tex_background.getSize().y + 1;
+	rows = _max_zoom / bgSize.x + 1;
+	colls = (_max_zoom * (_cam->getView().getSize().x / _cam->getView().getSize().y)) / bgSize.y + 1;
 	for (int i = 0; i < rows * colls; i++)
 		_sprites.push_back(p->addComponent<SpriteComponent>());
 
-	offset = -Vector2f(_max_zoom / 2.0f, _max_zoom / (_cam->getView().getSize().x / _cam->getView().getSize().y) / 2.0f);
+	topLeft = -Vector2f(_max_zoom / 2.0f, (_max_zoom / (_cam->getView().getSize().x / _cam->getView().getSize().y)) / 2.0f);
 
-	for (int i = 0; i < colls; i++)
-		for (int j = 0; j < rows; j++)
+	for (int i = 0; i < rows; i++)
+		for (int j = 0; j < colls; j++)
 		{
-			_sprites[i * j + i]->getSprite().setTexture(ResourceManager::Tex_background);
-			_sprites[i * j + i]->getSprite().setOrigin(128, 128);
+			_sprites[i * colls + j]->getSprite().setTexture(ResourceManager::Tex_background);
+			_sprites[i * colls + j]->getSprite().setOrigin(bgSize.x / 2, bgSize.y / 2);
+			Vector2f x = Vector2f(1, 1);
 			if (i % 2 == 1)
-				_sprites[i * j + i]->getSprite().setScale(-1.0f, 1.0f);
+				x.x = -1;
 			if (j % 2 == 1)
-				_sprites[i * j + i]->getSprite().setScale(1.0f, -1.0f);
+				x.y = -1;
+			_sprites[i * colls + j]->getSprite().setScale(x);
 		}
 }
 
@@ -38,9 +42,10 @@ BackgroundBuilderComponent::~BackgroundBuilderComponent()
 
 void BackgroundBuilderComponent::update(double dt)
 {
-	for (int i = 0; i < colls; i++)
-		for (int j = 0; j < rows; j++)
-			_sprites[i * j + i]->getSprite().setPosition(Vector2f(i * ResourceManager::Tex_background.getSize().x
-				, j * ResourceManager::Tex_background.getSize().y)
-				+ _cam->getMidpoint() + offset);
+	Vector2f offset = Vector2f(fmod(_cam->getMidpoint().x, bgSize.x * 2), fmod(_cam->getMidpoint().y, bgSize.y * 2));
+	for (int i = 0; i < rows; i++)
+		for (int j = 0; j < colls; j++)
+			_sprites[i * colls + j]->getSprite().setPosition(Vector2f(j * bgSize.x
+				, i * bgSize.y)
+				+ _cam->getMidpoint() + topLeft - offset);
 }
