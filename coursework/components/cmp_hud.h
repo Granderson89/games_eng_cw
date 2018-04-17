ef6411163d0df81455989dedd0e08fdadc1b6049
Paulius Bieksa <priverstinis@gmail.com>
2018-04-17 12:48:42 +0100

    Get component moved to constructor.

--- coursework/components/cmp_hud.h
+++ coursework/components/cmp_hud.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include "ecm.h"
+#include "cmp_player_state.h"
+#include "cmp_weapon_component.h"
+#include "cmp_camera.h"
+#include "cmp_text.h"
 
 class HudComponent : public Component {
 private:
@@ -21,6 +25,16 @@ private:
 	float _cooldownGap;
 	float _cooldownScale;
 	float _timerScale;
+
+	std::shared_ptr<CameraControllerComponent> cameraComp;
+	std::shared_ptr<PlayerStateComponent> state;
+	std::vector<std::shared_ptr<WeaponComponent>> weapons;
+	std::shared_ptr<TextComponent> healthText;
+	std::shared_ptr<ShapeComponent> healthBarBg;
+	std::shared_ptr<ShapeComponent> healthBar;
+	std::shared_ptr<SpriteComponent> weaponTypeSpr;
+	std::vector<std::shared_ptr<SpriteComponent>> cooldownSpr;
+	std::vector<std::shared_ptr<ShapeComponent>> timerSpr;
 
 
 public:
